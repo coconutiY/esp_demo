56f81bfc29e518826e0e08d67f1dcda1a4cc6003
Panmid <axs3372vdy@163.com>
2025-12-25 16:03:54 +0800

    init

---
+++ main/app_main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+}
